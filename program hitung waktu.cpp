77ded4024d92f6cab4dd517fca8a1d55958204e2
Rivaldo2117051054 <rivaldorr547@gmail.com>
2021-11-29 11:08:45 +0700

    program menghitung waktu

---
+++ program hitung waktu.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    int ak,dtk,mnt,jm;
+    
+    cin>>ak;
+    //masukan waktu yang ingin di cek
+    jm=ak/3600;
+    mnt=ak/60;
+    dtk=ak/1;
+    dtk=ak%60;
+    cout<<jm<<" jam "<<mnt<<" menit "<<dtk<<" detik "<<endl;
+}
